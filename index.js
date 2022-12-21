fc8cea269693bbeef599965ede35ded2f1c8ae8f
Colton Brenneman <colton.brenneman@gmail.com>
2022-12-21 14:22:17 -0700

    commit tree 85e749d09dbbf905435f3804d165ca2970ee0bdf

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const names = ['Guadalupe', 'Ollie', 'Aki']
+function writeCards(names, event){
+    const messages = []
+    for (let i = 0; i < names.length; i++){
+        messages.push( `Thank you, ${names[i]}, for the wodnerful ${event} gift!`)
+    }
+
+    return messages
+}
+
+writeCards(names, 'surprise')
+
+function countDown(num){
+    do {
+        console.log(num)
+        num--
+    }while (num >= 0)
+}
+
+countDown(4)
